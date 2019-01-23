0e63dda45e0973fce3e1469044bc704d19b846a8
Yahlunna <jomontger@gmail.com>
2019-01-23 12:37:39 +0000

    Initial commit

---
+++ Assets/Materials/wololoo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class wololoo : MonoBehaviour {
+
+	public Material rojo;
+	public Material azul;
+
+
+
+
+	/*void PointerEnter(){
+		GetComponent<Renderer> ().material = rojo;	
+		Debug.Log ("test1 - Enter");
+	}
+
+	void PointerClick(){
+		GetComponent<Renderer> ().material = azul;	
+		Debug.Log ("test2 - Click");
+	}
+
+	void PointerExit(){
+		Debug.Log ("test3 - Salida");
+		GetComponent<Renderer> ().material = rojo;
+	}*/
+
+
+	public void ColorRojo(){
+		GetComponent<Renderer> ().material = rojo;	
+	}
+
+	public void ColorAzul(){
+		GetComponent<Renderer> ().material = azul;	
+	}
+
+
+}
